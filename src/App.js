6b544fa69333aa0ac2a5d867da8cefcf312d2339
Kyle Runge <krunge1@hotmail.com>
2023-05-07 20:50:33 -0500

    used useState and update ages onClick

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import PersonCard from './components/PersonCard';
 function App() {
   return (
     <div className="App">
-      <PersonCard firstName={"Jack"} lastName={"Frost"} age={"18"} hairColor={"Blue"}/>
-      <PersonCard firstName={"Bob"} lastName={"Smith"} age={"50"} hairColor={"Blonde"}/>
-      <PersonCard firstName={"Ginger"} lastName={"Logitech"} age={"75"} hairColor={"Red"}/>
-      <PersonCard firstName={"Amazon"} lastName={"Business-Movers"} age={"50"} hairColor={"Black"}/>
+      <PersonCard firstName={"Jack"} lastName={"Frost"} age={18} hairColor={"Blue"}/>
+      <PersonCard firstName={"Bob"} lastName={"Smith"} age={25} hairColor={"Blonde"}/>      
+      <PersonCard firstName={"Ginger"} lastName={"Logitech"} age={75} hairColor={"Red"}/>
+      <PersonCard firstName={"Amazon"} lastName={"Business-Movers"} age={50} hairColor={"Black"}/>
     </div>
   );
 }
--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,14 +1,17 @@
 //rafce is the shorthand to push the below data
 
-import React from 'react'
+import React, {useState} from 'react'
 
 const PersonCard = (person) => {
     const {firstName, lastName, age, hairColor} = person;
+    const [birthday, setBirthday] = useState(age);
+
     return (
         <div>
             <h1>{lastName}, {firstName}</h1>
-            <p>Age: {age}</p>
+            <p>Age: {birthday}</p>
             <p>Hair Color: {hairColor}</p>
+            <button onClick = {eventSetter => setBirthday(birthday+1)}>Increase Age</button>
         </div>
     );
 
