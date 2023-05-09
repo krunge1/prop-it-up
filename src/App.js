07335ce4b73d540ac2925104722b3a1adb0025d7
Kyle Runge <107510109+krunge1@users.noreply.github.com>
2023-05-08 18:55:14 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <PersonCard firstName={"Jack"} lastName={"Frost"} age={18} hairColor={"Blue"}/>
       <PersonCard firstName={"Bob"} lastName={"Smith"} age={25} hairColor={"Blonde"}/>      
-      <PersonCard firstName={"Ginger"} lastName={"Logitech"} age={75} hairColor={"Red"}/>
+      <PersonCard firstName={"Ginger"} lastName={"Busch"} age={75} hairColor={"Red"}/>
       <PersonCard firstName={"Amazon"} lastName={"Business-Movers"} age={50} hairColor={"Black"}/>
     </div>
   );
